77b869f2caaf39734fe2125ff925cffb7a980e92
sfidahussain <sanfidahussain@gmail.com>
2021-10-01 16:40:20 -0500

    adding stuff
    not sure if necessary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
+import {onAuthUIStateChange, CognitoUserInterface, AuthState} from '@aws-amplify/ui-components'
+import API from '@aws-amplify/api';
+import { FormBuilder } from '@angular/forms';
+import {Auth} from "aws-amplify";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,55 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'amplify-auth';
+  user: CognitoUserInterface | undefined;
+  authState!: AuthState;
+  petForm = this.formBuilder.group({
+    name: ''
+  });
+  pets: any;
+
+  apiName = 'petstore';
+  path = '/petstore/pets/1';
+
+  constructor(private ref: ChangeDetectorRef, private formBuilder: FormBuilder) {
+  }
+
+  ngOnInit() {
+    onAuthUIStateChange((authState, authData) => {
+      this.authState = authState;
+      this.user = authData as CognitoUserInterface;
+      this.ref.detectChanges();
+    })
+    API.get(this.apiName, this.path, {}).then(response => {
+      // Add your code here
+      this.pets = response;
+      console.log(response);
+    });
+  }
+
+  async onSubmit(): Promise<void> {
+    const myInit = {
+      body: {
+        "name": this.petForm.get('name')?.value
+      }, // replace this with attributes you need
+      headers: {
+        Authorization: `Bearer ${(await (Auth.currentSession())).getIdToken().getJwtToken()}`,
+      }, // OPTIONAL
+    };
+
+    API.put(this.apiName, this.path, myInit).then(response => {
+      console.log('successful POST');
+    }).catch(error => {
+      console.log(error.response);
+    });
+    API.get(this.apiName, this.path, {}).then(response => {
+      // Add your code here
+      this.pets = response;
+      console.log(response);
+    });
+  }
+
+  ngOnDestroy() {
+    return onAuthUIStateChange;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,33 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import 'zone.js';
+import Amplify, {Auth} from 'aws-amplify';
+import awsconfig from './aws-exports';
+
+// Amplify.configure(awsconfig);
+Amplify.configure({
+  // OPTIONAL - if your API requires authentication
+  Auth: {
+    // REQUIRED - Amazon Cognito Identity Pool ID
+    identityPoolId: 'us-east-1:8ddf44cf-7bf3-424e-ab06-6bde8dd366f3',
+    // REQUIRED - Amazon Cognito Region
+    region: 'us-east-1',
+    // OPTIONAL - Amazon Cognito User Pool ID
+    userPoolId: 'us-east-1_kTEhTWvsf',
+    // OPTIONAL - Amazon Cognito Web Client ID (26-char alphanumeric string)
+    userPoolWebClientId: '6njk8md4n5o9d6e9jfisfqkm6c',
+  },
+  API: {
+    endpoints: [
+      {
+        name: "petstore",
+        endpoint: "https://bi6mfa71pb.execute-api.us-east-1.amazonaws.com"
+      },
+    ]
+  }
+});
+Auth.configure(awsconfig);
 
 if (environment.production) {
   enableProdMode();
